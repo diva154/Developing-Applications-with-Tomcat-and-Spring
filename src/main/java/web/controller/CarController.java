790105599ac2c3f883813879ebb7b7cc6627dca0
Divya <divyaarmarkar@gmail.com>
2026-09-21 14:43:40 +0530

    Please check

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -2,6 +2,7 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +22,7 @@ public class CarController {
     }
 
     @GetMapping(value = "/cars")
-    public String printCars(@RequestParam(value = "count",required = false)Integer count, ModelMap model){
+    public String getCars(@RequestParam(value = "count",required = false)Integer count, Model model){
         model.addAttribute("cars",carService.getCars(count));
         return "cars";
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -4,26 +4,32 @@ import org.springframework.stereotype.Service;
 import web.Car;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImpl implements CarService{
     private final List<Car> carList;
+    private final CarService carService;
 
-    public CarServiceImpl(){
+    public CarServiceImpl(CarService carService){
         carList = new ArrayList<>();
         carList.add(new Car("BMW",3,2021));
         carList.add(new Car("KIA",2,2013));
         carList.add(new Car("Mercedes",1,2014));
         carList.add(new Car("Audi",7,2020));
         carList.add(new Car("Fiat",4,2000));
+        this.carService = carService;
     }
     @Override
     public List<Car>getCars(Integer count){
-        if (count == null || count >=5){
+        if (count == null || count >= carList.size()){
     return carList;
 
+        }
+        if(count < 0){
+            return Collections.emptyList(); //returns an empty list for negative integers
         }
         return carList.stream().limit(count).collect(Collectors.toList());
     }
